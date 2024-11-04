e9b1705f33795e4b4a265b67b082ea13ffa3b1e7
Danny Su <danny.su@hytechc.com>
2024-11-04 12:51:01 +0800

    fix wront ret

--- auth.py
+++ auth.py
@@ -21,19 +21,19 @@ class EKSClient:
         print(f"got {len(clusters)} clusters")
         return clusters
 
-    def authenticate_eks_cluster(self, cluster_name) -> bool:
+    def authenticate_eks_cluster(self, cluster_name) -> None:
         commands = ['aws', 'eks', 'update-kubeconfig',
             '--name', cluster_name,
             '--region', self.region,
             '--profile', self.profile_name]
         if(self.dry_run):
             print(' '.join(commands))
-            return True
         else:
             # Update kubeconfig using the retrieved cluster details
             result = subprocess.check_output(commands)
             output = result.decode('utf-8')
-            return 'error' in output.lower()
+            if "Updated context" not in output:
+                raise Exception(output)
 
 def main(args):
     profiles = get_profiles()
@@ -47,11 +47,12 @@ def main(args):
         cluster_names = client.get_eks_clusters_for_profile()
         for cluster_name in cluster_names:
             print(f"Authenticating EKS cluster '{cluster_name}' using profile '{profile}'")
-            is_sucess = client.authenticate_eks_cluster(cluster_name)
-            if is_sucess:
+            try:
+                client.authenticate_eks_cluster(cluster_name)
                 print(f"Successfully authenticated and configured kubeconfig for cluster '{cluster_name}' using profile '{profile}'")
-            else:
+            except Exception as e:
                 print(f"Failed to authenticate and configure kubeconfig for cluster '{cluster_name}' using profile '{profile}'")
+                print(f"Error: {e}")
 
     except Exception as e:
         print(f"Error processing profile '{profile}': {e}")
